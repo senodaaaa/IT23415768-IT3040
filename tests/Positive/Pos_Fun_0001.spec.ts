55566428e1887cf3485d3db5b27edac30a9d5b96
Himal Nethmina <himal.nethmina2003@gmail.com>
2026-01-31 08:44:35 +0530

    inititalizing the Project

---
+++ tests/Positive/Pos_Fun_0001.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('Pos_Fun_0001 - Simple greeting conversion', async ({ page }) => {
+  await page.goto('https://www.swifttranslator.com/');
+
+  // Input
+  const singlishInput = page.getByRole('textbox', {
+    name: 'Input Your Singlish Text Here.'
+  });
+
+  // Enter Singlish
+  await singlishInput.fill('mama bath kanavaa.');
+
+  // Wait until Sinhala text appears
+  const sinhalaOutput = page.getByText('මම බත් කනවා.');
+
+  await expect(sinhalaOutput).toBeVisible();
+});
